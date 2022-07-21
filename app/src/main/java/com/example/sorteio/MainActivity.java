77d32522bad8a49187ac9e9ff2e3b08555a1bc6c
thiagosilva2004 <playerthiago3@gmail.com>
2022-07-20 21:07:48 -0300

    frist commit

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao(View v){
+        int numeroRandom = new Random().nextInt(11);
+
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("O número sorteado foi: " + numeroRandom);
+    }
+    
+
+}
